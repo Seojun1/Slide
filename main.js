5d78df91707b6444b4e0f3a611fb7787e125f663
LeeSeoJun <77569680+Seojun1@users.noreply.github.com>
2021-02-19 20:34:18 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.btn1').addEventListener('click', function () {
+    document.querySelector('.container').style.transform = 'translate(-10vw)';
+        });
+
+document.querySelector('.btn2').addEventListener('click', function () {
+    document.querySelector('.container').style.transform = 'translate(-100vw)';
+        });
+
+document.querySelector('.btn3').addEventListener('click', function () {
+    document.querySelector('.container').style.transform = 'translate(-200vw)';
+        });
